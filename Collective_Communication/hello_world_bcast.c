fa74dd87f23f67e7e9d4284a0f753702a0868a20
poonkuzhali <poonkuzhali1996@gmail.com>
2024-09-15 22:26:44 -0400

    Added Comments

--- Collective_Communication/hello_world_bcast.c
+++ Collective_Communication/hello_world_bcast.c
@@ -13,10 +13,13 @@ int main(int argc, char **argv)
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  // Root process sets the broadcast message
   if (rank == root)
   {
     strcpy(message, "Hello, world");
   }
+
+  // Message broadcasted to other processes
   MPI_Bcast(message, 12, MPI_CHAR, root, MPI_COMM_WORLD);
   printf( "Message from process %d : %s\n", rank, message);
 
